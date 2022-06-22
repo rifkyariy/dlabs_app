63487aba9f85106fcf104485a7d13b414b355c5f
Annurdien Rasyid <ajwadax@gmail.com>
2022-06-23 01:19:53 +0700

    chore: add utils

--- lib/app/core/utils/utils.dart
+++ lib/app/core/utils/utils.dart
@@ -1,3 +1,28 @@
+import 'package:get/get_utils/src/extensions/internacionalization.dart';
 import 'package:logger/logger.dart';
 
 final logger = Logger();
+
+extension IntX on num {
+  String get convertDayFromToday {
+    if (this == 0) {
+      return 'today'.tr;
+    } else if (this >= 30) {
+      // its integer division
+      int countMonth = this ~/ 30;
+      if (countMonth == 1) {
+        return '$countMonth ' + 'monthago'.tr;
+      } else {
+        return '$countMonth ' + 'monthsago'.tr;
+      }
+    } else if (this >= 120) {
+      return 'longtimeago'.tr;
+    } else {
+      if (this == 1) {
+        return '$this ' + 'dayago'.tr;
+      } else {
+        return '$this ' + 'daysago'.tr;
+      }
+    }
+  }
+}
