92be2d136924ee3ec297e2e5662e4859e9321ddf
Annurdien Rasyid <ajwadax@gmail.com>
2022-06-23 01:19:47 +0700

    chore : add constants

--- lib/app/core/utils/constants.dart
+++ lib/app/core/utils/constants.dart
@@ -1,2 +1,9 @@
 enum APPKEYBOARDTYPE { name, email, password, date }
 // TODO change tis
+
+class Constants {
+  static const femalePic =
+      "https://cdn.discordapp.com/attachments/900022715321311259/913815656770711633/app-profile-picture-female.png";
+  static const malePic =
+      "https://cdn.discordapp.com/attachments/900022715321311259/911343059827064832/app-profile-picture.png";
+}
